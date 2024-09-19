a944dbd8ded736b3c67685a1086e31ff1c3ac259
Pirate-Emperor <piratekingrahul@gmail.com>
2024-09-19 23:16:00 +0100

    Add docker\hive\hiveserver2\startup.sh

---
+++ docker/hive/hiveserver2/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# ${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false --hiveconf hive.root.logger=DEBUG,console
+${HIVE_HOME}/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false --hiveconf hive.root.logger=WARNING,console
